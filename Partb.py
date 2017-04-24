a31629e97fd6a64650c02fbff3a2266373a4acfe
DebrajGhose <dg.chaos06@gmail.com>
2017-04-24 16:51:13 -0400

    Script to generate log graph

---
+++ Partb.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 24 16:05:03 2017
+
+@author: dg144
+"""
+
+
+from pylab import *
+
+sizes = [5, 10, 20, 30, 40, 50, 75, 100, 200, 500]
+
+CVmax = [0.0]*len(sizes)
+
+count = 0
+
+for size in sizes:
+    
+    
+    A = np.load( 'CV' + '_' + str(size) + '.npy')
+    
+    CVmax[count] = max(A)/(size**2)
+    
+    count += 1
+    
+plot(sizes,CVmax,color='#7515e1')
+
+plot(sizes,CVmax,'.',color='#1875e1')
+
+plt.yscale('log')
+
+plt.xscale('log')
